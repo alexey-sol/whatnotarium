1d53beffc1bc0af467bf13d5d5246bb69b23de87
Alexey Soloviev <alexey.soloviev.w@gmail.com>
2021-08-08 19:57:43 +0300

    Client. Turn off React devtools on production

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,9 +3,9 @@ import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
 import React from "react";
 import ReactDOM from "react-dom";
+import { persistor, store } from "redux/store";
 
 import App from "components/layout/App";
-import { persistor, store } from "redux/store";
 
 const WrappedApp = (
     <Provider store={store}>
